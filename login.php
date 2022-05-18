097c0b186f9f041c0b8a1daba7c44991ce6d931b
Mad-Alchemist <105469951+Mad-Alchemist@users.noreply.github.com>
2022-05-18 14:18:16 +0900

    수정7

--- login.php
+++ login.php
@@ -12,7 +12,6 @@
     {
         $_SESSION['id']=$row['id'];
         $_SESSION['name']=$row['name'];
-        echo "<script>location.replace('loggedin_main.php');</script>";
         mysqli_close();
     }
     else
@@ -22,3 +21,26 @@
         mysqli_close();
     }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<script src="./js/Login.js"></script>
+<title> AYU-LIFE </title>
+</head>
+<link rel="stylesheet" href="./css/a.css" type="text/css">
+<link rel="stylesheet" href="./css/login.css" type="text/css">
+<body style="background-color:black;">
+<nav id="TopBar">
+  <a class="Uname"><?php $_SESSION['name'] ?>님 반갑습니다.</a>
+<ul>
+  <li><a class="Link" href="Lobby.html">Main</a></li>
+  <li><a class="Link" href="Lunch.html">Lunch</a></li>
+  <li><a class="Link" href="car.html">Go to Univ</a></li>
+  <li><a class="Link" href="Market.html">Market</a></li>
+  <li><a class="Link" href="Community.html">Community</a></li>
+  <li><a class="Link" id="btl" href="logout.php">Logout</a></li>
+</ul>
+</nav>
+</body>
+</html>
