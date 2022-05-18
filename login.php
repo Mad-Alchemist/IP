bb2ce02af0694d4642c8778eddbf1cef91011ae9
Mad-Alchemist <105469951+Mad-Alchemist@users.noreply.github.com>
2022-05-18 14:19:39 +0900

    수정8

--- login.php
+++ login.php
@@ -12,35 +12,13 @@
     {
         $_SESSION['id']=$row['id'];
         $_SESSION['name']=$row['name'];
-        mysqli_close();
+        echo "<script>location.replace('loggedin_main.php');</script>";
+        mysqli_close($con);
     }
     else
     {
         echo "<script>alert('아이디 혹은 비밀번호를 확인해 주십오')</script>";
         echo "<script>location.replace('main.php');</script>";
-        mysqli_close();
+        mysqli_close($con);
     }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<script src="./js/Login.js"></script>
-<title> AYU-LIFE </title>
-</head>
-<link rel="stylesheet" href="./css/a.css" type="text/css">
-<link rel="stylesheet" href="./css/login.css" type="text/css">
-<body style="background-color:black;">
-<nav id="TopBar">
-  <a class="Uname"><?php $_SESSION['name'] ?>님 반갑습니다.</a>
-<ul>
-  <li><a class="Link" href="Lobby.html">Main</a></li>
-  <li><a class="Link" href="Lunch.html">Lunch</a></li>
-  <li><a class="Link" href="car.html">Go to Univ</a></li>
-  <li><a class="Link" href="Market.html">Market</a></li>
-  <li><a class="Link" href="Community.html">Community</a></li>
-  <li><a class="Link" id="btl" href="logout.php">Logout</a></li>
-</ul>
-</nav>
-</body>
-</html>
