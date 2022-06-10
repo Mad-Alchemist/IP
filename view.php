913f1d4f8d9da312dd5c8b128f0f81b8777607f3
Mad-Alchemist <105469951+Mad-Alchemist@users.noreply.github.com>
2022-06-11 02:35:30 +0900

    css patch

--- view.php
+++ view.php
@@ -3,106 +3,17 @@
 
 <head>
     <meta charset='utf-8'>
-
-    <style>
-        .read_table {
-            border: 1px solid #444444;
-            margin-top: 30px;
-        }
-
-        .read_title {
-            height: 45px;
-            font-size: 23.5px;
-            text-align: center;
-            background-color: #3C3C3C;
-            color: white;
-            width: 1000px;
-        }
-
-        .read_id {
-            text-align: center;
-            background-color: #EEEEEE;
-            width: 30px;
-            height: 33px;
-        }
-
-        .read_id2 {
-            background-color: white;
-            width: 60px;
-            height: 33px;
-            padding-left: 10px;
-        }
-
-        .read_hit {
-            background-color: #EEEEEE;
-            width: 30px;
-            text-align: center;
-            height: 33px;
-        }
-
-        .read_hit2 {
-            background-color: white;
-            width: 60px;
-            height: 33px;
-            padding-left: 10px;
-        }
-
-        .read_content {
-            padding: 20px;
-            border-top: 1px solid #444444;
-            height: 500px;
-        }
-
-        .read_btn {
-            width: 700px;
-            height: 200px;
-            text-align: center;
-            margin: auto;
-            margin-top: 50px;
-        }
-
-        .read_btn1 {
-            height: 50px;
-            width: 100px;
-            font-size: 20px;
-            text-align: center;
-            background-color: white;
-            border: 2px solid black;
-            border-radius: 10px;
-        }
-
-        .read_comment_input {
-            width: 700px;
-            height: 500px;
-            text-align: center;
-            margin: auto;
-        }
-
-        .read_text3 {
-            font-weight: bold;
-            float: left;
-            margin-left: 20px;
-        }
-
-        .read_com_id {
-            width: 100px;
-        }
-
-        .read_comment {
-            width: 500px;
-        }
-    </style>
+    <link rel="stylesheet" href="./css/view.css" type="text/css">
 </head>
 
 <body>
     <?php
     $connect = mysqli_connect('127.0.0.1', 'root', '', 'web');
     $number = $_GET['number'];  // GET 방식 사용
-    $query = "select title, content, date, hit, name, id from board where number = $number";
+    $query = "select title, content, date, hit, name, id from board where number = $number and b_num=4";
     $result = $connect->query($query);
     $rows = mysqli_fetch_assoc($result);
-    $b_num=$_POST['b_num'];
-    $connect->query("update board set hit=hit+1 where number=$number");
+    $uphit="update board set hit +=1;"
     ?>
 
     <table class="read_table" align=center>
@@ -125,16 +36,12 @@
 
     <!-- MODIFY & DELETE 추후 세션처리로 보완 예정 -->
     <div class="read_btn">
-        <form method="post" action="./board.php" style="display:inline;">
-            <input type="hidden" name="b_num" value="<?php echo $b_num ?>">
-            <button type="submit" class="read_btn1">목록</button>&nbsp;&nbsp;
-        </form>
+        <button class="read_btn1" onclick="location.href='./board.php'">목록</button>&nbsp;&nbsp;
         <?php
         if (isset($_SESSION['id']) && $_SESSION['id'] == $rows['id']) { ?>
             <button class="read_btn1" onclick="location.href='./modify.php?number=<?= $number ?>'">수정</button>&nbsp;&nbsp;
             <button class="read_btn1" a onclick="ask();">삭제</button>
-        
-        
+
             <script>
             function ask() {
                 if (confirm("게시글을 삭제하시겠습니까?")) {
